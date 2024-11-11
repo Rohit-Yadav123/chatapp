aabdc489b5be480a29b431a24a9c3c4c2db7154e
rohit <rohit@ubuntu22.myguest.virtualbox.org>
2024-11-12 00:31:20 +0530

    changed the database from dbsqlite to postgresql

--- task/settings.py
+++ task/settings.py
@@ -119,10 +119,21 @@ WSGI_APPLICATION = 'task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat_msg_db',  # Replace with your database name
+        'USER': 'messageuser',  # Replace with your PostgreSQL user
+        'PASSWORD': 'message@2024',  # Replace with your PostgreSQL password
+        'HOST': 'localhost',  # Or the IP address if the DB is on a remote server
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
